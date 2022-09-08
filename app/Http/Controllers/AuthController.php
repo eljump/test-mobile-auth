49f464e88eb0a9beffdfb1f0527e27961adf82b4
Elias <eliasejump@gmail.com>
2022-09-08 23:03:41 +0300

    add: base login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        //todo: валидация
+        $phone = $request->input('phone');
+        //todo: выбор режима
+        $user = User::wherePhone($phone)->first();//todo: getByPhone, seeder
+        $code = random_int(1000, 9999);
+        Cache::put($phone, Hash::make($code), 3 * 60);
+        //todo
+        //SmsService::create($phone, $code);
+
+        //todo
+        return response(['code' => $code]);
+    }
+
+    public function smsSend()
+    {
+
+    }
+}
